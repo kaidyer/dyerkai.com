c3eada17049ae21b124b3599de2cf383ed169152
kaidyer <kaidyer29@gmail.com>
2025-03-09 15:56:10 -0400

    Add resume display and base template for website

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,8 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    resume = [
+        "SWE @ Company 1",
+        "SWE Intern @ Company 2"
+    ]
+    return render_template('index.html', resume=resume)
